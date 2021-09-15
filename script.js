0b3cee39a30bc50ab64715bfcade970cb73b0ee9
Rahmat <rfajri255@gmail.com>
2021-09-15 10:15:47 +0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const video = document.getElementById('webcam');        // Stream
-const liveView = document.getElementById('liveView');   // Container
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
 const demosSection = document.getElementById('demos');
 const enableWebcamButton = document.getElementById('webcamButton');
 
@@ -39,6 +39,20 @@ function enableCam(event) {
     video.addEventListener('loadeddata', predictWebcam);
   });
 }
+
+// Store the resulting model in the global scope of our app.
+var model = undefined;
+
+// Before we can use COCO-SSD class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment
+// to get everything needed to run.
+// Note: cocoSsd is an external object loaded from our index.html
+// script tag import so ignore any warning in Glitch.
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel;
+  // Show demo section now model is ready to use.
+  demosSection.classList.remove('invisible');
+});
 
 var children = [];
 
@@ -82,20 +96,4 @@ function predictWebcam() {
     window.requestAnimationFrame(predictWebcam);
   });
 }
-
-// Store the resulting model in the global scope of our app.
-var model = undefined;
-
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment
-// to get everything needed to run.
-// Note: cocoSsd is an external object loaded from our index.html
-// script tag import so ignore any warning in Glitch.
-cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove('invisible');
-});
-
-
 
